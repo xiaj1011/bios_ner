06a371e92c062afa2cf6fb60ca7967b08de82c04
xiajun <xiajun@idea.edu.cn>
2022-01-20 15:58:12 +0800

    Update download scripts, avoid downloading repeatedly

--- pretrain/download_pubmedbert.sh
+++ pretrain/download_pubmedbert.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+dst=$HOME/models/pubmedbert_abs
+if [ ! -d $dst ];
+then
+    echo "create directory $dst"
+    mkdir -p $dst
+else
+    echo "$dst already exists, skip download"
+    exit 0
+fi
+
 git lfs install
-git clone https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract pubmedbert_abs
+git clone https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract $dst
 
--- train/train.sh
+++ train/train.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 
 output='../example/data/ner_train'
-
 if [ ! -d $output ]; then 
     mkdir ${output}
 fi
 
+# download pretrained models firstly if they are not exist. Refer to download scripts from ../pretrain/
+ln -s $HOME/models/pubmedbert_abs ../pretrain/pubmedbert_abs
 cp ../example/configs/train.json.example ${output}/hf_argument.json
 
 CUDA_VISIBLE_DEVICES=1,2 python run_ner.py ${output}/hf_argument.json
