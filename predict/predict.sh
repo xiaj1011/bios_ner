da749b3f5e7da84e964145ef81adfd56cba2f716
xiajun <xiajun@idea.edu.cn>
2022-01-20 16:32:31 +0800

    Update predict.sh

--- predict/predict.sh
+++ predict/predict.sh
@@ -6,6 +6,6 @@ if [ ! -d $output ]; then
     mkdir ${output}
 fi
 
+# make sure you have trained model if you use this example script. Refer to train.sh from ../train
 cp ../example/configs/predict.json.example ${output}/hf_argument.json
-
 CUDA_VISIBLE_DEVICES=1 python predict.py ${output}/hf_argument.json
